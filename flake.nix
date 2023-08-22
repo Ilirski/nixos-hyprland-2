3aeae5561ea84c67d180cbc8ee3ff3399f5dd3ed
Illyria <24876805+Ilirski@users.noreply.github.com>
2023-08-22 19:53:44 +0800

    Add files via upload

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "Your new nix config";
+
+  inputs = {
+    # Nixpkgs
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    # Home manager
+    home-manager.url = "github:nix-community/home-manager/release-23.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    # Hyprland
+    hyprland.url = "github:hyprwm/Hyprland";
+
+  };
+
+  outputs = { nixpkgs, home-manager, hyprland, ... }@inputs: {
+    # NixOS configuration entrypoint
+    # Available through 'nixos-rebuild --flake .#your-hostname'
+    nixosConfigurations = {
+      user = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; }; # Pass flake inputs to our config
+        # > Our main nixos configuration file <
+        modules = [ 
+	 ./nixos/configuration.nix 
+	 hyprland.nixosModules.default
+	];
+      };
+    };
+
+    # Standalone home-manager configuration entrypoint
+    # Available through 'home-manager --flake .#your-username@your-hostname'
+    homeConfigurations = {
+      # FIXME replace with your username@hostname
+      "your-username@your-hostname" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+        extraSpecialArgs = { inherit inputs; }; # Pass flake inputs to our config
+        # > Our main home-manager configuration file <
+        modules = [ ./home-manager/home.nix ];
+      };
+    };
+  };
+}
---
+++ overlays.nix
@@ -0,0 +1,31 @@
+{ config, pkgs, ... }:
+let
+  wrap = program: exe: pkgs.symlinkJoin rec {
+    name = program.name;
+    paths = [ program ];
+    buildInputs = [ pkgs.makeWrapper ];
+    postBuild = ''
+      wrapProgram $out/bin/${exe} --set __NV_PRIME_RENDER_OFFLOAD 1 --set __NV_PRIME_RENDER_OFFLOAD_PROVIDER NVIDIA-G0 --set __GLX_VENDOR_LIBRARY_NAME nvidia --set __VK_LAYER_NV_optimus NVIDIA_only
+    '';
+  };
+in
+{
+  nixpkgs.overlays = [
+    (self: super:
+      {
+        #chromium = super.chromium.override {
+        #  commandLineArgs = [
+        #    "--use-gl=desktop"
+        #  ];
+        #};
+        #google-chrome-beta = super.google-chrome-beta.override {
+        #  commandLineArgs = [
+        #    "--use-gl=desktop"
+        #  ];
+        #};
+
+        microsoft-edge-dev = wrap super.microsoft-edge-dev "microsoft-edge-dev";
+      })
+  ];
+
+}
